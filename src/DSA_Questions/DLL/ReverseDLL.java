940b3324d8d74bef962cab425cf3bd51d691af8f
Mks30d <manish30d@gmail.com>
2025-01-06 01:04:35 +0530

    DLL

---
+++ src/DSA_Questions/DLL/ReverseDLL.java
@@ -0,0 +1,29 @@
+package DSA_Questions.DLL;
+
+public class ReverseDLL {
+
+
+
+//    public DLLNode reverseDLL(DLLNode head) {
+//        // Your code here
+//        DLLNode start=head, end=head;
+//        while(end.next != null) {
+//            end = end.next;
+//        }
+//
+//        while(start != end || end.next != start ) {
+//            int temp = start.data;
+//            start.data = end.data;
+//            end.data = temp;
+//
+//            start = start.next;
+//            end = end.prev;
+//        }
+//
+//        return head;
+//    }
+
+    public static void main(String[] args) {
+
+    }
+}
