34a32f5df630aaee7668097c1537b60ee6ad233a
Mks30d <manish30d@gmail.com>
2025-07-01 01:00:17 +0530

    Binary Tree Traversal and finding Height

--- src/Binary_Trees/BinaryTree_CRUD.java
+++ src/Binary_Trees/BinaryTree_CRUD.java
@@ -15,13 +15,13 @@ public class BinaryTree_CRUD {
 
     static Scanner sc = new Scanner(System.in);
 
-    // -------- Binary Tree Creation --------
+    // 1. -------- Binary Tree Creation --------
     private static Node createTree() {
-        Node root = null;
-        System.out.print("Enter data or -1 to terminate: ");
+        System.out.print("Enter data: ");
         int data = sc.nextInt();
+        System.out.println();
         if (data == -1) return null;
-        root = new Node(data);
+        Node root = new Node(data);
 
         System.out.println("Enter left for: " + data);
         root.left = createTree();
@@ -32,8 +32,68 @@ public class BinaryTree_CRUD {
         return root;
     }
 
+    // 2. -------- Binary Tree Traversals --------
+    public static void inOrder(Node root) {
+        if (root == null) return;
+
+        inOrder(root.left);
+        System.out.print(root.data + ", ");
+        inOrder(root.right);
+    }
+
+    public static void preOrder(Node root) {
+        if (root == null) return;
+
+        System.out.print(root.data + ", ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void postOrder(Node root) {
+        if (root == null) return;
+
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + ", ");
+    }
+
+    // -------- Binary Tree Height --------
+    public static int heightBT(Node root) {
+        if (root == null) return 0;
+
+        return Math.max(heightBT(root.left), heightBT(root.right)) + 1;
+    }
+
+    // -------- main function --------
     public static void main(String[] args) {
 
         Node root = createTree();
+
+        System.out.println("Binary Tree height: " + heightBT(root));
+
+        System.out.println("\nInOrder Traversal: ");
+        inOrder(root);
+        System.out.println("\nPreOrder Traversal: ");
+        preOrder(root);
+        System.out.println("\nPostOrder Traversal: ");
+        postOrder(root);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
